7dc443109b8a19ea37f9ba219ddfea42ab9c9537
Vadim Vereketa <kam1cadzee@gmail.com>
2019-12-31 22:54:43 +0200

    connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
+import "reflect-metadata";
 import express from 'express';
+import {createConnection} from 'typeorm';
+import config from './config';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+let connect = '';
+
 app.get('/', (req, res) => {
-  res.send(process.env.RESPONSE || 'NoName');
+  res.send(process.env.RESPONSE || connect);
 });
 
-app.listen(PORT, () => {
-  console.log('Start server, listing: localhost:', PORT)
-})
+createConnection(config.DATABASE_URL)
+  .then(() => {
+    console.log('Connect to DB');
+    connect = 'connect';
+
+    app.listen(PORT, () => {
+      console.log('Start server, listing: localhost:', PORT)
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+    console.log('Error. Connect to DB')
+  });
