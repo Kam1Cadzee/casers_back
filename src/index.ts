b7df07290c92c61777a563a7a0e2764a7c79938a
Vadim Vereketa <kam1cadzee@gmail.com>
2020-01-08 22:08:27 +0200

    prod4

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.static('client/build'));
+app.use(express.static('client/dist'));
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client/build/index.html'))
+  res.sendFile(path.join(__dirname, '..', 'client/dist/index.html'))
 });
 app.use('/transports', transport);
 
