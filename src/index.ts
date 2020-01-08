b1267829bb132cfb01335c98d572f6708acc0b78
Vadim Vereketa <kam1cadzee@gmail.com>
2020-01-08 17:34:48 +0200

    prob1

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,18 @@ import {createConnection, getConnection} from 'typeorm';
 import config from './config';
 import transport from './controller/transport';
 import {Transport} from './entity/Transport';
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-let connect = '';
-
+app.use(cors());
+app.use(express.static('client/build'));
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.send(connect);
+  res.sendFile(path.join(__dirname, '..', 'client/build/index.html'))
 });
 app.use('/transports', transport);
 
